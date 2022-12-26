e60dd79274d884e69f3101a46e5e64f0f9ff4273
김성은 <kse012050@naver.com>
2022-12-27 01:23:24 +0900

    자주 묻는 질문 완료

--- js/common.js
+++ js/common.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
+    // 스크롤 메뉴 배경
     $(window).scroll(function(e){
         $(this).scrollTop() > 0 ? $('header').addClass('background') : $('header').removeClass('background');
     })
 
+    // 스크롤 업 & 다운
     $(window).on('mousewheel',function(e){
         let delta = e.originalEvent.wheelDelta;
         delta > 0 ? $('header').addClass('active') : $('header').removeClass('active')
-        
+    })
+
+    // 자주 묻는 질문
+    $('.FAQPage .contentArea ul li button').click(function(){
+        $(this).toggleClass('active');
+        $(this).stop().next().slideToggle();
     })
 });
