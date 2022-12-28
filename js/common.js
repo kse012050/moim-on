527162553c721a80b0928b21215143fbbe54e147
김성은 <kse012050@naver.com>
2022-12-28 17:35:31 +0900

    도입문의 셀렉트 만들었다

--- js/common.js
+++ js/common.js
@@ -1,32 +1,67 @@
 $(document).ready(function(){
-    const mobileSize = 768;
 
-    console.log($(window).scrollTop());
-    let scrollPosition = $(window).scrollTop();
-    // 스크롤 메뉴 배경
-    $(window).scroll(function(e){
-        $(this).scrollTop() > 0 ? $('header').addClass('background') : $('header').removeClass('background');
-        $(this).scrollTop() - scrollPosition < 0 ? $('header').addClass('active') : $('header').removeClass('active')
-        scrollPosition = $(this).scrollTop();
-    })
+    // 스크롤시 메뉴 
+    scrollMenu();
+    // 텝
+    tabEvent();
+    // 모바일 메뉴
+    mobileMenu();
+    // 도입문의 셀릭트
+    inquirySelect();
 
+    function scrollMenu(){
+        let scrollPosition = $(window).scrollTop();
+        // 스크롤 메뉴 배경
+        $(window).scroll(function(e){
+            $(this).scrollTop() > 0 ? $('header').addClass('background') : $('header').removeClass('background');
+            $(this).scrollTop() - scrollPosition < 0 ? $('header').addClass('active') : $('header').removeClass('active')
+            scrollPosition = $(this).scrollTop();
+        })
+    }
 
-    $('.tabBtn ul li').click(function(){
-        $('.tabBtn ul li').removeClass('active');
-        $(this).addClass('active');
-        $('.tabContents div').removeClass('active');
-        $('.tabContents div').eq($(this).index()).addClass('active');
-    })
-  
+    // 텝 이벤트
+    function tabEvent(){
+        $('.tabBtn ul li').click(function(){
+            $('.tabBtn ul li').removeClass('active');
+            $(this).addClass('active');
+            $('.tabContents div').removeClass('active');
+            $('.tabContents div').eq($(this).index()).addClass('active');
+        })
+    }
+
+    // 도입문의 셀릭트
+    function inquirySelect(){
+        $('select').each(function(){
+            let selectSelector = $(this);
+            $(this).find('option').each(function(i){
+                i === 0 ? 
+                    selectSelector.siblings('.selectBox').append(`${$(this)[0].label}`):
+                    selectSelector.siblings('.selectList').append(`<div>${$(this)[0].label}</div>`);
+                console.log($(this));
+    
+            })
+        })
+    
+        $('.selectBox').click(function(){
+            $(this).siblings('.selectList').stop().slideToggle();
+        })
+    
+        $('.selectList div').click(function(){
+            $(this).parent().siblings('select').find('option').removeAttr('selected')
+            $(this).parent().siblings('select').find('option').eq($(this).index() + 1).attr('selected','selected')
+            $(this).parent().siblings('.selectBox').addClass('active');
+            $(this).parent().siblings('.selectBox').html($(this).html())
+            $(this).parent().slideUp();
+        })
+    }
 
     // 자주 묻는 질문
-    $('.FAQPage .contentArea ul li button').click(function(){
-        $(this).toggleClass('active');
-        $(this).stop().next().slideToggle();
-    })
-
-
-    mobileMenu();
+    function FAQDropdown(){
+        $('.FAQPage .contentArea ul li button').click(function(){
+            $(this).toggleClass('active');
+            $(this).stop().next().slideToggle();
+        })
+    }
 
     function mobileMenu(){
         $('nav').on('touchmove , mousewheel',function(e){
@@ -49,5 +84,4 @@ $(document).ready(function(){
             $('nav').removeAttr('style')
         })
     }
-
 });
